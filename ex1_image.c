cbbadffce79f56ce9a4a00b104a0aa9496d39dad
Renam <castro.renam@gmail.com>
2025-11-03 19:17:14 -0300

    example 1, capture image and show it via webserver

---
+++ ex1_image.c
@@ -0,0 +1,263 @@
+
+// https://dronebotworkshop.com/esp32-cam-microsd/
+// https://github.com/espressif/esp32-camera
+
+// Camera libraries
+#include "esp_camera.h"
+// #include "soc/soc.h"
+//#include "soc/rtc_cntl_reg.h"
+//#include "driver/rtc_io.h"
+
+// MicroSD Libraries
+#include "FS.h"
+#include "SD_MMC.h"
+
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
+// EEPROM Library
+#include "EEPROM.h"
+
+// Use 1 byte of EEPROM space
+#define EEPROM_SIZE 1
+
+// Counter for picture number
+unsigned int pictureCount = 0;
+
+// Pin definitions for CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+
+#ifndef LED_GPIO_NUM 4
+#define LED_GPIO_NUM 4
+# endif
+
+void setupLedFlash() {
+#if defined(LED_GPIO_NUM)
+  ledcAttach(LED_GPIO_NUM, 5000, 8);
+#else
+  log_i("LED flash is disabled -> LED_GPIO_NUM undefined");
+#endif
+}
+
+const char* ssid = "xyz";
+const char* password = "xyz";
+
+
+void server_setup(String path);
+AsyncWebServer server(80);
+
+void configESPCamera()  
+{
+
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_VGA; //FRAMESIZE_UXGA;  FRAMESIZE_HD FRAMESIZE_VGA
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming  PIXFORMAT_GRAYSCALE PIXFORMAT_JPEG
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_VGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_VGA;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+
+}
+
+void initMicroSDCard() 
+{
+
+  Serial.println("Mounting MicroSD Card");
+  if (!SD_MMC.begin()) {
+    Serial.println("MicroSD Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("No MicroSD Card found");
+    return;
+  }
+
+}
+
+
+void takeNewPhoto(String path) 
+{
+  // Setup frame buffer
+  camera_fb_t  * fb = esp_camera_fb_get();
+  
+  if (!fb) 
+  {
+    Serial.println("Camera capture failed\n");
+    return;
+  }
+
+  Serial.printf("Image width: %zu\n", fb->width);
+  Serial.printf("Image height: %zu\n", fb->height);
+  Serial.printf("Image len: %zu\n", fb->len);
+  Serial.printf("pixel format: %d\n", fb->format);
+
+
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if (!file) 
+  {
+    Serial.println("Failed to open file in write mode");
+  }
+  else 
+  {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+    file.close();
+  }
+
+  // Return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
+  delay(1000);
+
+}
+
+void setup() 
+{
+
+  // Start Serial Monitor
+  Serial.begin(115200);
+  initWiFi();
+
+  // Initialize the camera
+  Serial.print("Initializing the camera module...\n");
+  configESPCamera();
+  Serial.println("Camera OK!");
+
+  // Initialize the MicroSD
+  Serial.print("Initializing the MicroSD card module...\n");
+  initMicroSDCard();
+
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+  //EEPROM.write(0, 0); // reset 
+  pictureCount = EEPROM.read(0) + 1;
+
+  // Path where new picture will be saved in SD Card
+  String path = "/image_" + String(pictureCount) + ".jpg";
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  // Take and Save Photo
+  takeNewPhoto(path);
+  server_setup(path);
+
+  // Update EEPROM picture number counter
+  EEPROM.write(0, pictureCount);
+  EEPROM.commit();
+
+}
+
+void initWiFi() 
+{
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.print("Connected at IP: ");
+  Serial.print(WiFi.localIP());
+  Serial.print("\n");
+
+}
+
+
+
+void server_setup(String path)
+{
+  fs::FS &fs = SD_MMC;
+  
+  server.on("/", HTTP_GET, [path](AsyncWebServerRequest *request)
+  {
+    request->send(fs, path.c_str(), "image/jpg");
+  });
+  
+  server.begin();
+
+}
+
+void loop() 
+{
+
+}
